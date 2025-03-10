e7c1c10458665bb361ece56f12847a6d5a794066
Philip Brown <phil@bolthole.com>
2025-03-09 18:24:20 -0700

    added imports

--- modules/ui/CloudTab.py
+++ modules/ui/CloudTab.py
@@ -7,11 +7,11 @@ from PySide6.QtWidgets import (
 )
 from PySide6.QtCore import Qt
 
-# from modules.util.config.TrainConfig import TrainConfig
-# from modules.util.enum.CloudAction import CloudAction
-# from modules.util.enum.CloudFileSync import CloudFileSync
-# from modules.util.enum.CloudType import CloudType
-# from modules.util.ui.UIState import UIState
+from modules.util.config.TrainConfig import TrainConfig
+from modules.util.enum.CloudAction import CloudAction
+from modules.util.enum.CloudFileSync import CloudFileSync
+from modules.util.enum.CloudType import CloudType
+from modules.util.ui.UIState import UIState
 
 class CloudTab(QWidget):
     """
