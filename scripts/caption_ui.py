4b25daa8ccabc510b8044634cc0818da6246801b
Philip Brown <phil@bolthole.com>
2025-03-27 07:13:32 -0700

    set icon

--- scripts/caption_ui.py
+++ scripts/caption_ui.py
@@ -5,6 +5,8 @@ script_imports()
 
 from modules.ui.CaptionUI import CaptionUI
 from modules.util.args.CaptionUIArgs import CaptionUIArgs
+from modules.util.ui.ui_utils import set_window_icon
+
 
 import sys
 from PySide6.QtWidgets import QApplication
@@ -17,6 +19,8 @@ def handle_sigint(*args):
 def main():
     # signal.signal(signal.SIGINT, handle_sigint)
     app = QApplication(sys.argv)
+    set_window_icon(app)
+
 
     window = CaptionUI()
     window.showNormal()
